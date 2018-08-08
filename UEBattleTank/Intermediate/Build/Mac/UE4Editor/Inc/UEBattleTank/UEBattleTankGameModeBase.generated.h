3001b5bf6f2ad36d07f37329b995f4a689fdd0bf
Farid <faridbaik92@gmail.com>
2018-08-08 11:14:08 +0800

    landscape

---
+++ UEBattleTank/Intermediate/Build/Mac/UE4Editor/Inc/UEBattleTank/UEBattleTankGameModeBase.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef UEBATTLETANK_UEBattleTankGameModeBase_generated_h
+#error "UEBattleTankGameModeBase.generated.h already included, missing '#pragma once' in UEBattleTankGameModeBase.h"
+#endif
+#define UEBATTLETANK_UEBattleTankGameModeBase_generated_h
+
+#define UEBattleTank_Source_UEBattleTank_UEBattleTankGameModeBase_h_15_RPC_WRAPPERS
+#define UEBattleTank_Source_UEBattleTank_UEBattleTankGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define UEBattleTank_Source_UEBattleTank_UEBattleTankGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAUEBattleTankGameModeBase(); \
+	friend struct Z_Construct_UClass_AUEBattleTankGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AUEBattleTankGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/UEBattleTank"), NO_API) \
+	DECLARE_SERIALIZER(AUEBattleTankGameModeBase)
+
+
+#define UEBattleTank_Source_UEBattleTank_UEBattleTankGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAUEBattleTankGameModeBase(); \
+	friend struct Z_Construct_UClass_AUEBattleTankGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AUEBattleTankGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/UEBattleTank"), NO_API) \
+	DECLARE_SERIALIZER(AUEBattleTankGameModeBase)
+
+
+#define UEBattleTank_Source_UEBattleTank_UEBattleTankGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AUEBattleTankGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AUEBattleTankGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AUEBattleTankGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AUEBattleTankGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AUEBattleTankGameModeBase(AUEBattleTankGameModeBase&&); \
+	NO_API AUEBattleTankGameModeBase(const AUEBattleTankGameModeBase&); \
+public:
+
+
+#define UEBattleTank_Source_UEBattleTank_UEBattleTankGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AUEBattleTankGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AUEBattleTankGameModeBase(AUEBattleTankGameModeBase&&); \
+	NO_API AUEBattleTankGameModeBase(const AUEBattleTankGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AUEBattleTankGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AUEBattleTankGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AUEBattleTankGameModeBase)
+
+
+#define UEBattleTank_Source_UEBattleTank_UEBattleTankGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define UEBattleTank_Source_UEBattleTank_UEBattleTankGameModeBase_h_12_PROLOG
+#define UEBattleTank_Source_UEBattleTank_UEBattleTankGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	UEBattleTank_Source_UEBattleTank_UEBattleTankGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	UEBattleTank_Source_UEBattleTank_UEBattleTankGameModeBase_h_15_RPC_WRAPPERS \
+	UEBattleTank_Source_UEBattleTank_UEBattleTankGameModeBase_h_15_INCLASS \
+	UEBattleTank_Source_UEBattleTank_UEBattleTankGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define UEBattleTank_Source_UEBattleTank_UEBattleTankGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	UEBattleTank_Source_UEBattleTank_UEBattleTankGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	UEBattleTank_Source_UEBattleTank_UEBattleTankGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	UEBattleTank_Source_UEBattleTank_UEBattleTankGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	UEBattleTank_Source_UEBattleTank_UEBattleTankGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID UEBattleTank_Source_UEBattleTank_UEBattleTankGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ UEBattleTank/Source/UEBattleTank/UEBattleTankGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UEBattleTankGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEBATTLETANK_API AUEBattleTankGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
